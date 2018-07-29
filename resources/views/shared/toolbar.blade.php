1b4ee8647741be36a4594ab42ba3347f90960266
Robert Smits <robertsmits@hotmail.be>
2018-07-29 22:30:51 +0200

    Refinements

--- resources/views/shared/toolbar.blade.php
+++ resources/views/shared/toolbar.blade.php
@@ -1,7 +1,7 @@
-<md-toolbar hide-xs class="md-whiteframe-4dp">
+<md-toolbar hide show-gt-sm class="md-whiteframe-4dp">
     <div class="md-toolbar-tools">
         <h2>
-            <span>Product Key Management</span>
+            <span>Product Keys</span>
         </h2>
     </div>
     <div class="md-toolbar__search-container">
@@ -12,22 +12,22 @@
     </div>
 </md-toolbar>
 
-<md-toolbar show-xs hide-gt-xs class="md-whiteframe-4dp">
-    <div class="md-toolbar-tools" data-ng-hide="showMobileSearch">
+<md-toolbar hide-gt-sm class="md-whiteframe-4dp">
+    <div class="md-toolbar-tools" ng-hide="showMobileSearch">
         <h2>
-            <span>Product Key Management</span>
+            <span>Product Keys</span>
         </h2>
         <span flex></span>
-        <md-button class="md-icon-button" aria-label="Search" data-ng-click="showMobileSearch = true">
+        <md-button class="md-icon-button" aria-label="Search" ng-click="showMobileSearch = true">
             <md-icon>search</md-icon>
         </md-button>
     </div>
-    <div class="md-toolbar-tools" data-ng-show="showMobileSearch">
-        <md-button class="md-icon-button" aria-label="Back" data-ng-click="app.clearSearch(); showMobileSearch = false">
+    <div class="md-toolbar-tools" ng-show="showMobileSearch">
+        <md-button class="md-icon-button" aria-label="Back" ng-click="app.clearSearch(); showMobileSearch = false">
             <md-icon>arrow_back</md-icon>
         </md-button>
     </div>
-    <div class="md-toolbar__search-container" data-ng-show="showMobileSearch">
+    <div class="md-toolbar__search-container" ng-show="showMobileSearch">
         <div class="md-toolbar__input-wrapper">
             <input class="md-toolbar__input" ng-model="app.search" placeholder="Search products" ng-change="app.loadProducts()">
         </div>
